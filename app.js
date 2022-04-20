612cfbdd22011860ec8b1ee6d6e1138348b7dc9d
SirAlyon <alessiosada04@gmail.com>
2022-04-20 15:59:55 +0200

    add es and bonus

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus (fatelo):
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        h1_text: 'Welcome VueJS :)',
+        image_link: 'https://picsum.photos/200/300'
+    }
+})
